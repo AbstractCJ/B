5843c2bc9fa502c2a0d70872dddff35107f281d0
User <User@DESKTOP-6JPNDEN.lan>
2024-01-05 19:59:21 -0500

    added name

--- src/B/B.java
+++ src/B/B.java
@@ -2,6 +2,8 @@ package B;
 
 public class B {public static void main(String[] args) {
 	
+	//Add your name --> Cindy Garcia
+	
 	System.out.println("Here is hoping");
 	System.out.println("B");
 	System.out.println("Getting excited!");
